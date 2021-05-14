06b4f66612b92931fbbf9557d90a5915d1881c84
Laura <laura.a.chelaru@gmail.com>
2021-05-14 21:20:24 +0200

    update, added 5 new drawings

--- src/app/MichisDrawings.ts
+++ src/app/MichisDrawings.ts
@@ -1,42 +1,74 @@
 export const MichisDrawings = [
-    {
-      url: "dadi.png",
-      category: ["bw"]
-    },
-    {
-      url: "eye.png",
-      category: ["bw"]
-    },
-    {
-        url: "captainamerica.png",
-        category: ["tv-series-movies"]
-      },
-      {
-        url: "eleven-strangerthings.jpg",
-        category: ["tv-series-movies", "bw"]
-      },
-      {
-        url: "steve-strangerthings.jpg",
-        category: ["tv-series-movies", "bw"]
-      },
-      {
-        url: "flower.png",
-        category: ["other"]
-      },
-      {
-        url: "hands.png",
-        category: ["bw"]
-      },
-      {
-        url: "harrypotter.png",
-        category: ["tv-series-movies"]
-      },
-      {
-        url: "sailormoon.png",
-        category: ["cartoons"]
-      },
-      {
-        url: "parrot.png",
-        category: ["other"]
-      }
+  {
+    url: "hair.png",
+    category: ["bw"]
+  },
+  {
+    url: "wrinkles.png",
+    category: ["bw"]
+  },
+  {
+    url: "pores.png",
+    category: ["bw"]
+  },
+  {
+    url: "mouth-smile.png",
+    category: ["bw"]
+  },
+  {
+    url: "mouth.png",
+    category: ["bw"]
+  },
+  {
+    url: "eye-3quarti.png",
+    category: ["bw"]
+  },
+  {
+    url: "nose-3quarti.png",
+    category: ["bw"]
+  },
+  {
+    url: "nose-front.png",
+    category: ["bw"]
+  },
+  {
+    url: "steve-strangerthings.jpg",
+    category: ["tv-series-movies", "bw"]
+  },
+  {
+    url: "dadi.png",
+    category: ["bw"]
+  },
+  {
+    url: "eye.png",
+    category: ["bw"]
+  },
+  {
+    url: "captainamerica.png",
+    category: ["tv-series-movies"]
+  },
+  {
+    url: "eleven-strangerthings.jpg",
+    category: ["tv-series-movies", "bw"]
+  },
+  {
+    url: "flower.png",
+    category: ["other"]
+  },
+  {
+    url: "hands.png",
+    category: ["bw"]
+  },
+  {
+    url: "harrypotter.png",
+    category: ["tv-series-movies"]
+  },
+  {
+    url: "sailormoon.png",
+    category: ["cartoons"]
+  },
+  {
+    url: "parrot.png",
+    category: ["other"]
+  }
 ]
